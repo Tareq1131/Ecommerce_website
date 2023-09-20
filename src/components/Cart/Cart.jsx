3864e2d8b9cc91c64c145ca022b9343f5a7a407c
Tareq1131 <tareqislam076@gmail.com>
2023-09-20 19:13:59 +0600

    calculation

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -7,18 +7,22 @@ import './Cart.css';
     const {cart}=props;
   
     let total=0;
+    let totalshipping=0;
     for(const product of cart){
         total = total + product.price;
+        totalshipping = totalshipping + product.shipping;
     }
+    const text = total*7/100;
+    const grandTotal = total+totalshipping+text;
 
   return (
     <div className='cart'>
        <h4>order summery</h4>
         <p>Selected items:{cart.length}</p>
         <p>Total Price :${total}</p>
-        <p>Total Shpping: </p>
-        <p>Text:</p>
-        <h6>Grand Total: </h6>
+        <p>Total Shipping: ${totalshipping}</p>
+        <p>Text:${text.toFixed(2)}</p>
+        <h6>Grand Total: ${grandTotal.toFixed(2)} </h6>
     </div>
   )
 }
