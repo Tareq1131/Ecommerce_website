2d60883c537ffb26f79f683c25e7cf7d6ca1f88b
Tareq1131 <tareqislam076@gmail.com>
2023-11-03 17:01:20 +0600

    Create Login component and style Login form

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,12 +1,24 @@
 //import React from 'react';
-import './Login.css'
+import "./Login.css";
 
 const Login = () => {
-    return (
-        <div>
-            <h3>Login page</h3>
+  return (
+    <div className="form-container">
+      <h2 className="form-tittle">Login</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="">Email</label>
+          <input type="email" name="email" id="" required />
         </div>
-    );
+
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Login" />
+      </form>
+    </div>
+  );
 };
 
 export default Login;
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -1,12 +1,28 @@
 // import React from 'react';
-import './SignUp.css'
+import "./SignUp.css";
 
 const SignUp = () => {
-    return (
-        <div>
-            <h2>Please sign up</h2>
+  return (
+    <div className="form-container">
+      <h2 className="form-tittle">Sign Up</h2>
+      <form>
+        <div className="form-control">
+          <label htmlFor="">Email</label>
+          <input type="email" name="email" id="" required />
         </div>
-    );
+
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="password">Confirm Password</label>
+          <input type="password" name="confirm" id="" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Login" />
+      </form>
+    </div>
+  );
 };
 
 export default SignUp;
